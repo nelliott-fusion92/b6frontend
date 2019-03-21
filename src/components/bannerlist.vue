40af5c1b12304c5de43643e615b736ab9d9f638c
Nick Elliott <nelliott@liquidus.net>
2019-03-21 16:59:36 -0500

    adding salesforce

--- src/components/bannerlist.vue
+++ src/components/bannerlist.vue
@@ -21,6 +21,11 @@
         .then((response) => {
           this.banners = response.data
         })
+    },
+    methods: {
+      listClicked: function(e, x) {
+        console.log(e)
+      }
     }
   }
 
@@ -30,12 +35,29 @@
 
   <div>
     <bannerlistitem v-for="banner in banners" v-bind:banner="banner" urlRoot="banner" :key="banner._id" />
+    <div id="listbox-id-1" class="slds-dropdown slds-dropdown_length-5 slds-dropdown_fluid" role="listbox">
+      <ul class="slds-listbox slds-listbox_vertical" role="presentation">
+        <li v-for="banner in banners" v-bind:banner="banner" :key="banner._id" role="presentation" class="slds-listbox__item">
+          <div id="option1" class="slds-media slds-listbox__option slds-listbox__option_plain slds-media_small" role="option">
+            <span class="slds-media__figure slds-listbox__option-icon"></span>
+            <span class="slds-media__body">
+              <span @click="listClicked(banner.name)"class="slds-truncate" :title="banner.name"> {{ banner.name }}</span>
+            </span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 
 </template>
 
-<style lang="scss" scoped>
+<style lang="css" scoped>
 
-  @import '../../assets/theme.scss';
+  @import url('../../node_modules/@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css');
+  #listbox-id-1 {
+    width: 300px;
+    left: 155px;
+    float: none;
+  }
 
 </style>
